05b28d9a1b4d43205d51033277d5d4568d48282f
LifeofCode <LifeofCode@users.noreply.github.com>
2015-07-23 15:54:31 +0000

    create organization/index erb and route

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,10 @@
 get '/' do
   erb :index
 end
+
+get '/students/:id/organizations' do 
+  #TODO: change when sessions/login/authorization is finished
+  @organizations = Organization.all
+  @student = Student.find(params[:id])
+  erb :'organizations/index'
+end
--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,7 @@ class Student < ActiveRecord::Base
 	has_many :favourites
 	has_many :organizations, through: :favourites
 
+  validates :email, uniqueness: true
+
 end
+
