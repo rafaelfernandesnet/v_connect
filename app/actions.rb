a8f3cf3ae2ee1d4a3da9582950bcac60213cb551
yianyue <yianyue@gmail.com>
2015-07-23 11:28:45 -0400

    comment out authorized helper

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,24 @@
 # Homepage (Root path)
+
+# TODO: authorize logged in student
+# helpers do
+#   def protected!
+#     return if authorized?
+#     headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+#     halt 401, "Not authorized\n"
+#   end
+
+#   def authorized?
+#     @student = Student.find_by(email: @email)
+#     @student.authenticate(@password)
+#   end
+# end
+
 get '/' do
-  erb :index
+  "Everybody can see this page"
 end
+
+# get '/protected' do
+#   protected!
+#   "Welcome, authenticated client"
+# end
