932919625313c5210a58574bdb5d40a1f684f9d1
vagrant <vagrant@lighthouse-labs>
2015-07-23 14:57:42 +0000

    Adds student/profile action, adds and links boostrap CSS to project

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,15 @@
 get '/' do
   erb :index
 end
+
+# this will route to a student's profile after they log in
+# expect session[:id] to be an id of a student to be set
+get 'students/profile' do
+	@student = Student.find_by(session[:id])
+
+	if @student
+		erb: '/students/show'
+	else redirect '/'
+	end
+
+end
