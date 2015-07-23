9691563eb0cd8f185f7f34fcc87b6863e885fc09
yianyue <yianyue@gmail.com>
2015-07-23 12:19:33 -0400

    added more validations to the student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,11 @@
 class Student < ActiveRecord::Base
-
-  has_secure_password
 	
 	has_many :favourites
 	has_many :organizations, through: :favourites
+
+  has_secure_password
+
+  validates :first_name, :last_name, presence: true
+  validates :email, format: {with: /\w+\.?\w+@\w+\.\w+/, message: "invalid"}, uniqueness: true
 
 end
