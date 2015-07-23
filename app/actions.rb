173c785644fce29e39e2546a40099bf930b9790b
LifeofCode <LifeofCode@users.noreply.github.com>
2015-07-23 17:14:30 +0000

    reorganize views

--- app/actions.rb
+++ app/actions.rb
@@ -3,16 +3,32 @@ get '/' do
   erb :index
 end
 
-get '/students/:id/organizations' do 
+#students can see a list of all organizations that are registered
+get '/organizations' do 
   #TODO: change when sessions/login/authorization is finished
   @organizations = Organization.all
-  @student = Student.find(params[:id])
   erb :'organizations/index'
 end
 
+#an organization can see a list of interested students
 get '/organizations/:id/students' do
   #TODO: refactor erb file using partials
   @organization = Organization.find(params[:id])
   @students = @organization.students
-  erb :'favourites/index'
+  erb :'students/index'
 end
+
+#display all students that are registered
+get '/students' do 
+  @students = Student.all
+  erb :'students/index'
+end
+
+#a student can see their favourite organizations
+get '/students/:id/organizations' do
+  @student = Student.find(params[:id])
+  @organizations = @student.organizations
+  erb :'organizations/index'
+end
+
+
