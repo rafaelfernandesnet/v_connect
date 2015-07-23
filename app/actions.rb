950358dad9d0301de7cdb04a871337d52cfddff1
yianyue <yianyue@gmail.com>
2015-07-23 13:15:15 -0400

    student can login

--- app/actions.rb
+++ app/actions.rb
@@ -1,55 +1,63 @@
 # Homepage (Root path)
 
 # TODO: authorize logged in student
-# helpers do
-#   def protected!
-#     return if authorized?
-#     headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
-#     halt 401, "Not authorized\n"
-#   end
-
-#   def authorized?
-#     @student = Student.find_by(email: @email)
-#     @student.authenticate(@password)
-#   end
-# end
-
-# before '/student/*' do
-#   authenticate!
-# end
-
-# get '/protected' do
-#   protected!
-#   "Welcome, authenticated client"
-# end
+helpers do
+  def current_student
+    Student.find(session[:id]) if session[:id]
+  end
+  def current_student?
+    session[:id].nil?
+  end
+end
 
 get '/' do
   erb :index
 end
 
+# temporary routes
 get '/registered' do
   'registered!'
 end
 
+get '/loggedin' do
+  'logged in!'
+end
+
 get '/student/register' do
   @student = Student.new
+  @errors = [] #TODO: create a helper for checking errors
   erb :'students/new'
 end
 
+get '/student/session' do
+  @student = Student.new
+  @errors = []
+  erb :'students/login'
+end
+
 post '/students' do
-  @errors = [] #TODO: create a helper for checking errors
-  @student = Student.new(
-    email: params[:email]
-  )
+  @errors = []
+  @student = Student.new(params[:student])
   @student.password = params[:password]
   @student.password_confirmation = params[:password2]
   if @student.save
-    # TODO: login
-    # TODO: redirect to the student profile page
+    # TODO: login and redirect to the student profile page
     redirect '/registered'
   else
     @errors = @student.errors.full_messages 
     erb :'students/new'
   end
+end
 
+post '/student/session' do
+  @student = Student.find_by(email: params[:email])
+  @errors = []
+  case
+  when @student.nil?
+    @errors << "Invalid login"
+    erb :'students/login'
+  when @student.authenticate(params[:password])
+    session[:id] = @student.id
+    redirect '/loggedin'
+  end
 end
